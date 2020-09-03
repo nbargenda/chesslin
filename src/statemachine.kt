58b8436cee3763cdcd21e32d9a9bdbbe82bbb76b
Nathalie Bargenda <n.bargenda@gmail.com>
2020-09-03 20:04:03 +0200

    more StateMachine

--- src/statemachine.kt
+++ src/statemachine.kt
@@ -1,9 +1,8 @@
 
-sealed class State(val name: String)
+data class State(val name: String)
 data class Input(val value: String)
 
 data class StateMachine(val states: Set<State>, val inputs: Set<Input>, val delta: (State, Input) -> State,
                         val initialState: State, val isFinalState: (State) -> Boolean){
 
 }
-
